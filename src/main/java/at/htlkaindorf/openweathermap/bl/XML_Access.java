41e06b00271df85e387ffcbcf03d672114364d8b
Simon <82031350+soesia18@users.noreply.github.com>
2022-11-09 17:29:33 +0100

    Update XML_Access.java

--- src/main/java/at/htlkaindorf/openweathermap/bl/XML_Access.java
+++ src/main/java/at/htlkaindorf/openweathermap/bl/XML_Access.java
@@ -12,7 +12,7 @@ import java.net.URI;
 public class XML_Access {
     public static Weather getWeatherFromCity(String city, String lang) throws FileNotFoundException {
         city = city.replace(" ", "%20");
-        URI uri = URI.create("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=f76fc912c1986b56ff6940c47c51186f&mode=xml&units=metric&lang=" + lang);
+        URI uri = URI.create("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=APP_KEY&mode=xml&units=metric&lang=" + lang);
         Weather weather = JAXB.unmarshal(uri, Weather.class);
         System.out.println(weather);
         return weather;
@@ -20,7 +20,7 @@ public class XML_Access {
 
     public static ForecastWeather getForecastWeatherFromCity(String city, String lang) throws FileNotFoundException {
         city = city.replace(" ", "%20");
-        URI uri = URI.create("http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=f76fc912c1986b56ff6940c47c51186f&mode=xml&units=metric&lang=" + lang);
+        URI uri = URI.create("http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=APP_KEY&mode=xml&units=metric&lang=" + lang);
         ForecastWeather weather = JAXB.unmarshal(uri, ForecastWeather.class);
         System.out.println(weather);
         return weather;
